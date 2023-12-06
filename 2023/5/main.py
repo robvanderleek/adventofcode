5439977c23c98ca3632781a5b298969f4f80c96f
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2023-12-06 23:20:29 +0100

    Testing range mappings

--- 2023/5/main.py
+++ 2023/5/main.py
@@ -31,7 +31,41 @@ def do_mappings(number, mappings):
                 break
     return number
 
+def do_range_mappings(ranges, mappings):
+    result = []
+    for mapping in mappings:
+        updated_ranges = []
+        for r in ranges:
+            updated_range = []
+            for m in mapping:
+                # no overlap
+                if m[1] < r[0] or m[0] > r[0] + (r[1] - 1):
+                    continue
+                # complete overlap
+                if m[0] <= r[0] and m[1] >= r[0] + (r[1] - 1):
+                    updated_range.append((r[0] + m[2], r[1]))
+                else:
+                    # left/middle/right overlap
+                    left_len = (m[0] - r[0])
+                    if left_len > 0:
+                        updated_range.append((r[0], left_len))
+                    middle_start = r[0] + max(left_len, 0)
+                    middle_len = min(r[0] + r[1], m[1]) - middle_start
+                    updated_range.append((middle_start + m[2], middle_len))
+                    right_len = (r[0] + r[1]) - m[1]
+                    if right_len > 0:
+                        updated_range.append((r[0], right_len))
+            if updated_range:
+                updated_ranges.extend(updated_range)
+            else:
+                updated_ranges.extend(r)
+        result = updated_ranges
+    return result
+
 [line_nr, seed_to_soil] = parse_mapping(lines, line_nr)
+
+print(do_range_mappings([(79, 14), (45, 11)], [seed_to_soil]))
+
 [line_nr, soil_to_fertilizer] = parse_mapping(lines, line_nr)
 [line_nr, fertilizer_to_water] = parse_mapping(lines, line_nr)
 [line_nr, water_to_light] = parse_mapping(lines, line_nr)
