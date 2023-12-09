52bfc911aa2f655d4c153688990621fd675264c8
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2023-12-09 10:44:00 +0100

    Day 9

--- 2023/9/main.py
+++ 2023/9/main.py
@@ -18,4 +18,15 @@ def next_value(history):
                 next_history.append(history[idx + 1] - n)
         return history[-1] + next_value(next_history)
 
+def previous_value(history):
+    if len([n for n in history if n == 0]) == len(history):
+        return 0
+    else:
+        next_history = []
+        for idx, n in enumerate(history):
+            if idx <= len(history) - 2:
+                next_history.append(history[idx + 1] - n)
+        return history[0] - previous_value(next_history)
+
 print(sum([next_value(h) for h in histories]))
+print(sum([previous_value(h) for h in histories]))
