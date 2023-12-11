77583a94b286f3076877778daf6c2d57c73291eb
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2023-12-11 19:22:51 +0100

    Day 11

--- 2023/11/main.py
+++ 2023/11/main.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 
 def load_universe():
-    with open('input-small.txt') as infile:
+    with open('input.txt') as infile:
         lines = infile.readlines()
-    universe = []
+    result = []
     for line in lines:
         patches = [c for c in line.strip()]
-        universe.append(patches)
-        if len([c for c in patches if c != '.']) == 0:
-            universe.append(patches)
-    columns = []
+        result.append(patches)
+    return result
+
+def get_empty_rows(universe):
+    result = []
+    for idx, row in enumerate(universe):
+        if len([c for c in row if c != '.']) == 0:
+            result.append(idx)
+    return result
+
+def get_empty_columns(universe):
+    result = []
     for x_idx in range(0, len(universe[0])):
         all_empty = True
         for y_idx in range(0, len(universe)):
@@ -17,24 +25,35 @@ def load_universe():
                 all_empty = False
                 break
         if all_empty:
-            columns.append(x_idx)
-    for row in universe:
-        for idx, col in enumerate(columns):
-            row.insert(col + idx, '.')
-    return universe
+            result.append(x_idx)
+    return result 
+
+def load_galaxies(universe):
+    result = []
+    for y_idx, row in enumerate(universe):
+        for x_idx, col in enumerate(row):
+            if col == '#':
+                result.append((x_idx, y_idx))
+    return result
+
+def expand(galaxies, empty_rows, empty_columns, factor):
+    result = []
+    for g in galaxies:
+        new_x = g[0] + (len([c for c in empty_columns if c < g[0]]) * (factor - 1))
+        new_y = g[1] + (len([r for r in empty_rows if r < g[1]]) * (factor - 1))
+        result.append((new_x, new_y))
+    return result
 
 universe = load_universe()
-
-locations = []
-for y_idx, row in enumerate(universe):
-    for x_idx, col in enumerate(row):
-        if col == '#':
-            locations.append((x_idx, y_idx))
+empty_rows = get_empty_rows(universe)
+empty_columns = get_empty_columns(universe)
+galaxies = load_galaxies(universe)
+galaxies = expand(galaxies, empty_rows, empty_columns, 1000000)
 
 pairs = []
-for idx, loc in enumerate(locations):
-    for i in range(idx + 1, len(locations)):
-        pairs.append((loc, locations[i]))
+for idx, loc in enumerate(galaxies):
+    for i in range(idx + 1, len(galaxies)):
+        pairs.append((loc, galaxies[i]))
 
 total = 0
 for p in pairs:
