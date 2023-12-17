c354052bac47ff22e4c233729af29ae94c3f26d7
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2023-12-17 09:07:15 +0100

    Day 12

--- 2023/12/main.py
+++ 2023/12/main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import re
-import functools
+from functools import cache
 
 rows = []
 
-with open('input.txt') as infile:
+with open('input-small.txt') as infile:
     lines = infile.readlines()
     for line in lines:
         parts = line.strip().split(' ')
@@ -12,7 +12,6 @@ with open('input.txt') as infile:
         groups = [int(g) for g in parts[1].split(',')]
         rows.append((springs, groups))
 
-@functools.lru_cache(maxsize=None)
 def generate(s):
     result = []
     if '?' not in s:
@@ -24,46 +23,40 @@ def generate(s):
 
 def is_valid(springs, groups):
     spring_groups = [len(p) for p in re.split('[.]+', springs) if p != '']
-    return spring_groups == groups
+    g = list(groups)
+    return spring_groups == g
 
+@cache
 def check(s, groups):
-    result = 0
-    if '?' not in s:
-        if is_valid(s, groups):
-            result += 1
+    if len(groups) == 0:
+        if '#' not in s:
+            return 1
+        else:
+            return 0
+    s = s.strip('.')
+    dot_index = s.find('.')
+    question_index = s.find('?')
+    if question_index >= 0 and (question_index < dot_index or dot_index < 0):
+        return \
+            check(s.replace('?', '#', 1), groups) + \
+            check(s.replace('?', '.', 1), groups)
     else:
-        result += check(s.replace('?', '#', 1), groups)
-        result += check(s.replace('?', '.', 1), groups)
-    return result
+        if dot_index < 0 and len(s) == groups[0]:
+            return check('', groups[1:])
+        elif dot_index == groups[0]:
+            return check(s[dot_index:], groups[1:])
+        else:
+            return 0
 
 total = 0
 for r in rows:
-    total += check(r[0], r[1])
-
-total = 0
-for r in rows:
-    row_total = 0
-    springs = r[0]
+    springs = ''
     groups = []
-    groups.extend(r[1])
-    valid = [s for s in generate(springs) if is_valid(s, groups)]
-    row_total += len(valid)
-    for _ in range(4):
+    for i in range(5):
+        springs += r[0]
+        springs += '?' if i < 4 else ''
         groups.extend(r[1])
-        next_valid = []
-        for v in valid:
-            next_valid.extend([s for s in generate(v + '.' + springs) if is_valid(s, groups)])
-            next_valid.extend([s for s in generate(v + '#' + springs) if is_valid(s, groups)])
-        valid = next_valid
-        row_total = len(next_valid)
-    print(row_total)
-    total += row_total
+    valid = check(springs, tuple(groups))
+    total += valid
 
-        #unfolded_springs += r[0] + '?'
-        #unfolded_groups.extend(r[1])
-    #unfolded_springs += r[0]
-    #unfolded_groups.extend(r[1])
-    #row_total += check(unfolded_springs, unfolded_groups)
-    #print(f'{unfolded_springs},{unfolded_groups}: {row_total}')
-    #total += row_total
 print(total)
