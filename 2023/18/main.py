d35347e6bae92b9d1321272144fd2882c188f7cb
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2023-12-19 18:12:53 +0100

    Day 18

--- 2023/18/main.py
+++ 2023/18/main.py
@@ -5,26 +5,10 @@ with open('input.txt') as infile:
     plan = []
     for line in lines:
         parts = line.strip().split(' ')
-        plan.append((parts[0], int(parts[1])))
-
-def dim(plan):
-    xmin = ymin = 0
-    xmax = ymax = 0
-    pos = [0, 0]
-    for d in plan:
-        if d[0] == 'R':
-            pos[0] += d[1]
-        elif d[0] == 'D':
-            pos[1] += d[1] 
-        elif d[0] == 'L':
-            pos[0] -= d[1]
-        elif d[0] == 'U':
-            pos[1] -= d[1] 
-        xmin = min(xmin, pos[0])
-        ymin = min(ymin, pos[1])
-        xmax = max(xmax, pos[0])
-        ymax = max(ymax, pos[1])
-    return ((xmax - xmin) + 1, (ymax - ymin) + 1), xmin, ymin 
+        hex = parts[2][2:-1]
+        dir = 'R' if hex[-1] == '0' else 'D' if hex[-1] == '1' else 'L' \
+            if hex[-1] == '2' else 'U'
+        plan.append((dir, int(hex[:-1], 16)))
 
 def shoelace(verts):
     totalX = 0
@@ -36,40 +20,24 @@ def shoelace(verts):
     totalY += verts[-1][1] * verts[0][0] 
     return abs(totalX - totalY) / 2
 
-def show(grid):
-    for line in grid:
-        print(''.join(line))
+def to_verts(plan):
+    pos = [0, 0]
+    verts = [(0, 0)]
+    for d in plan:
+        if d[0] == 'R':
+            pos[0] += d[1]
+            verts.append((pos[0], pos[1]))
+        if d[0] == 'D':
+            pos[1] += d[1]
+            verts.append((pos[0], pos[1]))
+        if d[0] == 'L':
+            pos[0] -= d[1]
+            verts.append((pos[0], pos[1]))
+        if d[0] == 'U':
+            pos[1] -= d[1]
+            verts.append((pos[0], pos[1]))
+    return verts
 
-dimensions = dim(plan)
-grid = []
-for y in range(dimensions[0][1]):
-    grid.append(['.' for x in range(dimensions[0][0])])
-
-pos = [0, 0]
-verts = [(0, 0)]
-for d in plan:
-    if d[0] == 'R':
-        for _ in range(d[1]):
-            pos[0] += 1
-            grid[pos[1]][pos[0]] = '#'
-        verts.append((pos[0], pos[1]))
-    if d[0] == 'D':
-        for _ in range(d[1]):
-            pos[1] += 1
-            grid[pos[1]][pos[0]] = '#'
-        verts.append((pos[0], pos[1]))
-    if d[0] == 'L':
-        for _ in range(d[1]):
-            pos[0] -= 1
-            grid[pos[1]][pos[0]] = '#'
-        verts.append((pos[0], pos[1]))
-    if d[0] == 'U':
-        for _ in range(d[1]):
-            pos[1] -= 1
-            grid[pos[1]][pos[0]] = '#'
-        verts.append((pos[0], pos[1]))
-
-show(grid)
-
+verts = to_verts(plan)
 trench = sum([d[1] for d in plan])
 print((shoelace(verts[:-1]) + (trench / 2)) + 1)
