91de3545ca9c14c8a6982aff2d551e769e1f096f
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2023-12-20 18:59:01 +0100

    Refactored

--- 2023/19/main.py
+++ 2023/19/main.py
@@ -13,9 +13,7 @@ with open('input.txt') as infile:
         if in_workflows:
             workflow = []
             val = line.strip()[:-1].split('{')
-            name = val[0]
-            logic = val[1]
-            workflows[name] = logic
+            workflows[val[0]] = val[1]
         else:
             part = {}
             for c in line.strip()[1:-1].split(','):
@@ -30,10 +28,7 @@ def parse_exp(exp):
     return [ch, operator, val]
 
 def revert(ch, operator, val):
-    if operator == '<':
-        return [ch, '>', val - 1]
-    else:
-        return [ch, '<', val + 1]
+    return [ch, '>', val - 1] if operator == '<' else [ch, '<', val + 1]
 
 def execute(workflow, part):
     rules = workflow.split(',')
@@ -65,8 +60,7 @@ for p in parts:
         result += p['x'] + p['m'] + p['a'] + p['s']
 print(result)
 
-nodes = []
-edges = []
+nodes = edges = []
 for k, v in workflows.items():
     if not k in nodes:
         nodes.append(k)
@@ -88,27 +82,18 @@ def walk(node, end, path, paths, nodes):
     else:
         for e in edges:
             if e[0] == node:
-                if type(e[2][0]) == list:
-                    walk(e[1], end, path + e[2], paths, nodes + [node])
-                else:
-                    walk(e[1], end, path + [e[2]], paths, nodes + [node])
+                walk(e[1], end, path + e[2], paths, nodes + [node])
     return paths
 
-bounds = []
-for p in walk('in', 'A', [], [], []):
-    p_bounds = {'x': [1, 4000], 'm': [1, 4000], 'a': [1, 4000], 's': [1, 4000]}
-    for b in p:
-        ch_bounds = p_bounds[b[0]]
-        if b[1] == '<' and ch_bounds[1] > b[2] - 1:
-            ch_bounds[1] = b[2] - 1
-        elif ch_bounds[0] < b[2] + 1:
-            ch_bounds[0] = b[2] + 1
-    bounds.append(p_bounds)
-
 total = 0
-for b in bounds:
-    total += (b['x'][1] - b['x'][0] + 1) * \
-            (b['m'][1] - b['m'][0] + 1) * \
-            (b['a'][1] - b['a'][0] + 1) * \
-            (b['s'][1] - b['s'][0] + 1)
+for p in walk('in', 'A', [], [], []):
+    b = {'x': [1, 4000], 'm': [1, 4000], 'a': [1, 4000], 's': [1, 4000]}
+    for pb in p:
+        ch_bounds = b[pb[0]]
+        if pb[1] == '<' and ch_bounds[1] > pb[2] - 1:
+            ch_bounds[1] = pb[2] - 1
+        elif ch_bounds[0] < pb[2] + 1:
+            ch_bounds[0] = pb[2] + 1
+    total += (b['x'][1] - b['x'][0] + 1) * (b['m'][1] - b['m'][0] + 1) * \
+             (b['a'][1] - b['a'][0] + 1) * (b['s'][1] - b['s'][0] + 1)
 print(total)
