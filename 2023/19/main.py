f46e680043f3b1b3fac85a92630b7d9b4c275ed0
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2023-12-20 18:50:28 +0100

    Day 19

--- 2023/19/main.py
+++ 2023/19/main.py
@@ -23,15 +23,25 @@ with open('input.txt') as infile:
                 part[val[0]] = int(val[1])
             parts.append(part)
 
+def parse_exp(exp):
+    ch = exp[0]
+    operator = exp[1]
+    val = int(exp[2:])
+    return [ch, operator, val]
+
+def revert(ch, operator, val):
+    if operator == '<':
+        return [ch, '>', val - 1]
+    else:
+        return [ch, '<', val + 1]
+
 def execute(workflow, part):
     rules = workflow.split(',')
     for rule in rules[:-1]:
         split = rule.split(':')
         exp = split[0]
         target = split[1]
-        ch = exp[0]
-        operator = exp[1]
-        val = int(exp[2:])
+        [ch, operator, val] = parse_exp(exp)
         if operator == '<':
             if part[ch] < val:
                 return target 
@@ -54,3 +64,51 @@ for p in parts:
     if process(p) == 'A':
         result += p['x'] + p['m'] + p['a'] + p['s']
 print(result)
+
+nodes = []
+edges = []
+for k, v in workflows.items():
+    if not k in nodes:
+        nodes.append(k)
+    rules = v.split(',')
+    exps = []
+    for rule in rules[:-1]: 
+        split = rule.split(':')
+        exp = split[0]
+        target = split[1]
+        [ch, op, val] = parse_exp(exp)
+        edges.append((k, target, exps + [[ch, op, val]]))
+        exps.append(revert(ch, op, val))
+    edges.append((k, rules[-1], exps))
+
+def walk(node, end, path, paths, nodes):
+    if node == end:
+        paths.append(path)
+        return paths
+    else:
+        for e in edges:
+            if e[0] == node:
+                if type(e[2][0]) == list:
+                    walk(e[1], end, path + e[2], paths, nodes + [node])
+                else:
+                    walk(e[1], end, path + [e[2]], paths, nodes + [node])
+    return paths
+
+bounds = []
+for p in walk('in', 'A', [], [], []):
+    p_bounds = {'x': [1, 4000], 'm': [1, 4000], 'a': [1, 4000], 's': [1, 4000]}
+    for b in p:
+        ch_bounds = p_bounds[b[0]]
+        if b[1] == '<' and ch_bounds[1] > b[2] - 1:
+            ch_bounds[1] = b[2] - 1
+        elif ch_bounds[0] < b[2] + 1:
+            ch_bounds[0] = b[2] + 1
+    bounds.append(p_bounds)
+
+total = 0
+for b in bounds:
+    total += (b['x'][1] - b['x'][0] + 1) * \
+            (b['m'][1] - b['m'][0] + 1) * \
+            (b['a'][1] - b['a'][0] + 1) * \
+            (b['s'][1] - b['s'][0] + 1)
+print(total)
