5c4c356fb9d9b89130243909abcb98c83f1e2987
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2023-12-24 01:36:35 +0100

    Day 21

--- 2023/21/main.py
+++ 2023/21/main.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
 grid = []
-positions = []
 with open('input.txt') as infile:
     lines = infile.readlines()
     for y, line in enumerate(lines):
         grid.append([c for c in line.strip()]) 
         if 'S' in line:
-            positions.append((y, line.index('S')))
+            S = (y, line.index('S'))
+
+free_positions = set()
+for y, row in enumerate(grid):
+    for x, col in enumerate(row):
+        if col == '.' or col == 'S':
+            free_positions.add((y, x))
 
 def show_grid(grid, positions):
     for y, row in enumerate(grid):
@@ -20,25 +25,32 @@ def show_grid(grid, positions):
         print(line)
 
 def free(pos):
-    return pos[0] >= 0 and pos[0] < len(grid) and pos[1] >= 0 and \
-        pos[1] < len(grid[0]) and grid[pos[0]][pos[1]] in ['.', 'S']
+   return (pos[0] % len(grid), pos[1] % len(grid[0])) in free_positions
 
-for _ in range(64):
-    new_positions = []
-    for p in positions:
-        south = (p[0] + 1, p[1])
-        if free(south) and not south in new_positions:
-            new_positions.append(south)
-        west = (p[0], p[1] - 1)
-        if free(west) and not west in new_positions:
-            new_positions.append(west)
-        north = (p[0] - 1, p[1])
-        if free(north) and not north in new_positions:
-            new_positions.append(north)
-        east = (p[0], p[1] + 1)
-        if free(east) and not east in new_positions:
-            new_positions.append(east)
-    positions = new_positions
+def calc(n):
+    positions = [S]
+    for _ in range(n):
+        new_positions = set()
+        for p in positions:
+            south = (p[0] + 1, p[1])
+            if free(south) and not south in new_positions:
+                new_positions.add(south)
+            west = (p[0], p[1] - 1)
+            if free(west) and not west in new_positions:
+                new_positions.add(west)
+            north = (p[0] - 1, p[1])
+            if free(north) and not north in new_positions:
+                new_positions.add(north)
+            east = (p[0], p[1] + 1)
+            if free(east) and not east in new_positions:
+                new_positions.add(east)
+        positions = new_positions
+    return len(positions)
 
-# show_grid(grid, positions)
-print(len(positions))
+r1 = calc(65)
+r2 = calc(65 + 131)
+r3 = calc(65 + 2 * 131)
+a = (r3 + r1 - (2 * r2)) / 2
+b = ((4 * r2) - (3 * r1) - r3) / 2
+c = r1
+print((a * pow(202300, 2)) + (b * 202300) + c)
