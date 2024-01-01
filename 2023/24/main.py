6e72737efa4ffc8b721f2ebcf34eb4a5f5032aff
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2024-01-01 19:20:13 +0100

    Day 24

--- 2023/24/main.py
+++ 2023/24/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import random
 
 hail = []
 with open('input.txt') as infile:
@@ -33,9 +34,34 @@ def collide(h1, h2):
     if t1 < 0 or t2 < 0:
         return None
     y = s1 * (x - x1) + y1
-    return(x, y)
+    return(x, y, int(t1))
+
+def update(hail, dvx, dvy):
+    return (hail[0], (hail[1][0] + dvx, hail[1][1] + dvy, hail[1][2]))
      
+def predict(hail, t, dvz):
+    return hail[0][2] + t * (hail[1][2] + dvz)
+
+def solve():
+    while True:
+        sel = random.choices(hail, k=4)
+        for dvx in range(-500, 500):
+            for dvy in range(-500, 500):
+                hail0 = update(sel[0], dvx, dvy)
+                c1 = collide(hail0, update(sel[1], dvx, dvy))
+                c2 = collide(hail0, update(sel[2], dvx, dvy))
+                c3 = collide(hail0, update(sel[3], dvx, dvy))
+                if c1 and c2 and c3 and c1[0] == c2[0] and c2[0] == c3[0] and \
+                    c1[1] == c2[1] and c2[1] == c3[1]:
+                    for dvz in range(-500, 500):
+                        z1 = predict(sel[1], c1[2], dvz)
+                        z2 = predict(sel[2], c2[2], dvz)
+                        z3 = predict(sel[3], c3[2], dvz)
+                        if z1 == z2 and z2 == z3:
+                            return c1[0] + c1[1] + z1
+
 total = 0
+
 minrange = 200000000000000
 maxrange = 400000000000000
 for p in pairs:
@@ -45,3 +71,4 @@ for p in pairs:
             pos[1] >= minrange and pos[1] <= maxrange:
             total += 1
 print(total)
+print(solve())
