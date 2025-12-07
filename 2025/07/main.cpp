17845d6eccf5bea3238db7ee06a5facd671eff53
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2025-12-07 21:46:57 +0100

    feat: ✨ Day 7

--- 2025/07/main.cpp
+++ 2025/07/main.cpp
@@ -1,6 +1,8 @@
 #include <cassert>
 #include <fstream>
 #include <iostream>
+#include <set>
+#include <map>
 #include <sstream>
 
 
@@ -15,21 +17,58 @@ std::vector<std::string> loadInput(const std::string &filename) {
 }
 
 void partOne() {
-    const auto ids = loadInput("/Users/rob/projects/robvanderleek/adventofcode/2025/07/input-small.txt");
+    const auto lines = loadInput("/Users/rob/projects/robvanderleek/adventofcode/2025/07/input.txt");
+    std::set<int> beams;
     long result = 0;
+    for (const auto &line : lines) {
+        for (int i = 0; i < line.length(); ++i) {
+            char c = line[i];
+            if (c == 'S') {
+                beams.insert(i);
+            } else if (c == '^' && beams.count(i)) {
+                result++;
+                beams.erase(i);
+                beams.insert(i - 1);
+                beams.insert(i + 1);
+            }
+        }
+    }
     std::cout << result << std::endl;
-    assert(result == 0);
+    assert(result == 1507);
+}
+
+long trackBeam(std::vector<std::string> lines, int i, int j, std::map<std::pair<int, int>, long> &p) {
+    if (i == lines.size() - 1) {
+        return 1;
+    }
+    if (lines[i][j] == '^') {
+        if (p.count({i, j})) {
+            return p.at({i, j});
+        }
+        long traces = trackBeam(lines, i + 1, j - 1, p) + trackBeam(lines, i + 1, j + 1, p);
+        p[{i, j}] = traces;
+        return traces;
+    }
+    return trackBeam(lines, i + 1, j, p);
 }
 
 void partTwo() {
-    const auto ids = loadInput("/Users/rob/projects/robvanderleek/adventofcode/2025/07/input-small.txt");
+    const auto lines = loadInput("/Users/rob/projects/robvanderleek/adventofcode/2025/07/input.txt");
+    std::map<std::pair<int, int>, long> p;
     long result = 0;
+    for (int j = 0; j < lines[0].length(); ++j) {
+        char c = lines[0][j];
+        if (c == 'S') {
+            result = trackBeam(lines, 0, j, p);
+            break;
+        }
+    }
     std::cout << result << std::endl;
-    assert(result == 0);
+    assert(result == 1537373473728);
 }
 
 int main() {
     partOne();
-    // partTwo();
+    partTwo();
     return 0;
 }
